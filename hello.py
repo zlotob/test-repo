6d6fc4cfb3a19f8d3bf463274ef47189654d8c38
Bartosz <zlotob@gmail.com>
2018-10-30 13:03:27 +0100

    kolejne zadania

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello world!")
+# -*- coding: utf-8 -*-
+szer = 42
+print("-" * szer)
+print("|  Czas  |     Zawodnik     |    Data    |")
+print("*" * szer)
+print("| {:6.4f} | {:16s} | {:10s} |" .format(9.58, "Usain Bolt", "16.08.2009"))
+print("| {:6.2f} | {:16s} | {:10s} |" .format(9.69, "Tyson Gay", "20.09.2009"))
+print("| {:6.3f} | {:16s} | {:10s} |" .format(9.69, "Yohan Blake", "23.09.2012"))
+print("| {:6.3f} | {:16s} | {:10s} |" .format(9.74, "Asafa Powell", "2.09.2008"))
+print("-" * szer)
---
+++ jednostki.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+slownik = {"twoja":"yo", "stara":"mama", 3:"trzy"}
+nowa_poz=input("co dodamy do slownika?")
+definicja=input("co to znaczy?")
+slownik[nowa_poz]=definicja
